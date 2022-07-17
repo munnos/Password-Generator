8283c7cb5d3775fc981a33769266396705765415
Michael Munn <munnos@hotmail.co.uk>
2022-07-17 14:51:38 +0100

    empty array and arrays with characters added

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordContainer = [];
+
+var letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var specialCharacters = [
+  "!",
+  "£",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "?",
+  "/",
+  '",',
+  ">",
+  "<",
+  "@",
+  "#",
+  "~",
+];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
