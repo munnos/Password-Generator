d2e4004c03ec064222cde04a7910bc5a1e9fc7e6
Michael Munn <munnos@hotmail.co.uk>
2022-07-18 09:16:16 +0100

    Started if blocks to populate password array

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {  var password = generatePassword();
+function writePassword() {
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -14,117 +15,120 @@ generateBtn.addEventListener("click", writePassword);
 
 //Start of code to generate password
 
-function makePassword() {
-
-
-var lengthofPassword = window.prompt(
-  "How many characters do you wish your password to contain?");
+function generatePassword() {
+  var lengthofPassword = window.prompt(
+    "How many characters do you wish your password to contain?"
+  );
 
   var userchoiceCharacters = parseInt(lengthofPassword);
 
   if (
     lengthofPassword < 8 ||
     lengthofPassword > 128 ||
-    isNaN(userchoiceCharacters) )
- { return alert("Please insert a number between 8 and 128");
-}
+    isNaN(userchoiceCharacters)
+  ) {
+    return alert("Please insert a number between 8 and 128");
+  }
 
-var userchoiceCaps = window.prompt(
-  "Do you wish to include capital letters in the password? Please enter yes or no"
-);
+  var userchoiceCaps = window.prompt(
+    "Do you wish to include capital letters in the password? Please enter yes or no"
+  );
 
+  var userchoiceSpecial = window.prompt(
+    "Do you wish to include special characters in the password? Please enter yes or no"
+  );
 
+  var userchoiceNumbers = window.prompt(
+    "Do you wish to include numbers in the password? Please enter yes or no"
+  );
 
-var userchoiceSpecial = window.prompt(
-  "Do you wish to include special characters in the password? Please enter yes or no"
-);
+  var lowerLetters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
-userchoiceSpecial = userchoiceSpecial.toUpperCase();
+  var upperLetters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
 
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-var userchoiceNumbers = window.prompt(
-  "Do you wish to include numbers in the password? Please enter yes or no"
-);
+  var specialCharacters = [
+    "!",
+    "£",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "?",
+    "/",
+    '",',
+    ">",
+    "<",
+    "@",
+    "#",
+    "~",
+  ];
 
-userchoiceNumbers = userchoiceNumbers.toUpperCase();
+  var passwordContainer = [];
 
+  var finishedPassword = [];
 
-
-var characters = window.prompt(
-  "How many characters do you wish your password to contain?"
-);
-
-
-
-
-
-
-
-var letters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-var specialCharacters = [
-  "!",
-  "£",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "?",
-  "/",
-  '",',
-  ">",
-  "<",
-  "@",
-  "#",
-  "~",
-];
-
-
-
-var passwordContainer = [];
-
-function characters();
-
-if (characters <= 128);
-
-
-
-
-var characters = window.prompt(
-  "How many characters do you wish your password to contain?"
-);
-
-characters();
+  if (userchoiceCaps === "yes") {
+    capsValue = lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+    finishedPassword.unshift(capsValue);
+    passwordContainer = passwordContainer.concat(lowerLetters);
+  } else null;
 }
